03707db14f44e3952e6c74925c9448081194d9e7
AJ <adwin596@gmail.com>
2020-10-11 21:30:14 +0000

    scipts to ci from local srslte folder and push locally

--- docker/build_sdr.sh
+++ docker/build_sdr.sh
@@ -1,5 +1,12 @@
 set -ex
 
-IMG="sdr-ubuntu-srslte-lime:v1"
+if [ "$#" -ne 1 ] ; then
+  echo "Need argument for tag" >&2
+  exit 1
+fi
+
+TAG=${1}
+#IMG="sdr-ubuntu-srslte-lime:v1"
+IMG="sdr-ubuntu-srslte-lime:${TAG}"
 docker build . -t \
   $IMG
