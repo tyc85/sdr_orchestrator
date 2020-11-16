8ecb8b482fb7b2a87119f568e408061010049632
tyc85 <steven.tychen@gmail.com>
2020-11-16 06:19:12 +0000

    minor mods on scripts

--- docker/push_sdr.sh
+++ docker/push_sdr.sh
@@ -1,10 +1,16 @@
 set -ex
 
-REMOTE_REPO="4141studio/sdr-ubuntu-srslte-lime"
-TAG="v1"
+if [ "$#" -ne 1 ] ; then
+  echo "Need argument for tag" >&2
+  exit 1
+fi
+
+TAG=${1}
+
+REMOTE_REPO="tyc85/sdr-ubuntu-srslte-lime"
 LOCAL_IMG="sdr-ubuntu-srslte-lime":$TAG
 REMOTE_IMG=$REMOTE_REPO:$TAG
 
-docker login --username=4141studio
+docker login 
 docker tag $LOCAL_IMG $REMOTE_IMG
 docker push $REMOTE_IMG
